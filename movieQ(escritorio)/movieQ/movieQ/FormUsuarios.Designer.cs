7a293495899f2d5d8d5c42412ed52ea28a80d595
davidac23 <d.almirall23@gmail.com>
2017-03-27 02:37:49 +0200

    añadir y eliminar usuarios

--- movieQ(escritorio)/movieQ/movieQ/FormUsuarios.Designer.cs
+++ movieQ(escritorio)/movieQ/movieQ/FormUsuarios.Designer.cs
@@ -30,15 +30,14 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormUsuarios));
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
-            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
-            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
             this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonVolver = new System.Windows.Forms.ToolStripButton();
             this.labelNombre = new System.Windows.Forms.Label();
             this.textBoxNombre = new System.Windows.Forms.TextBox();
             this.labelContraseña = new System.Windows.Forms.Label();
             this.textBoxContraseña = new System.Windows.Forms.TextBox();
             this.dataGridViewUsuarios = new System.Windows.Forms.DataGridView();
-            this.toolStripButtonVolver = new System.Windows.Forms.ToolStripButton();
+            this.button1 = new System.Windows.Forms.Button();
             this.toolStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewUsuarios)).BeginInit();
             this.SuspendLayout();
@@ -46,33 +45,13 @@
             // toolStrip1
             // 
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripButton1,
-            this.toolStripButton2,
             this.toolStripButton3,
             this.toolStripButtonVolver});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(222, 25);
+            this.toolStrip1.Size = new System.Drawing.Size(386, 25);
             this.toolStrip1.TabIndex = 0;
             this.toolStrip1.Text = "toolStrip1";
-            // 
-            // toolStripButton1
-            // 
-            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
-            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton1.Name = "toolStripButton1";
-            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
-            this.toolStripButton1.Text = "Añadir Usuario";
-            // 
-            // toolStripButton2
-            // 
-            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
-            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton2.Name = "toolStripButton2";
-            this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
-            this.toolStripButton2.Text = "Buscar Usuario";
             // 
             // toolStripButton3
             // 
@@ -82,6 +61,17 @@
             this.toolStripButton3.Name = "toolStripButton3";
             this.toolStripButton3.Size = new System.Drawing.Size(23, 22);
             this.toolStripButton3.Text = "Eliminar Usuario";
+            this.toolStripButton3.Click += new System.EventHandler(this.toolStripButton3_Click);
+            // 
+            // toolStripButtonVolver
+            // 
+            this.toolStripButtonVolver.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButtonVolver.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonVolver.Image")));
+            this.toolStripButtonVolver.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonVolver.Name = "toolStripButtonVolver";
+            this.toolStripButtonVolver.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButtonVolver.Text = "Volver";
+            this.toolStripButtonVolver.Click += new System.EventHandler(this.toolStripButtonVolver_Click);
             // 
             // labelNombre
             // 
@@ -118,26 +108,28 @@
             // dataGridViewUsuarios
             // 
             this.dataGridViewUsuarios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewUsuarios.Location = new System.Drawing.Point(16, 144);
+            this.dataGridViewUsuarios.Location = new System.Drawing.Point(16, 167);
             this.dataGridViewUsuarios.Name = "dataGridViewUsuarios";
-            this.dataGridViewUsuarios.Size = new System.Drawing.Size(184, 89);
+            this.dataGridViewUsuarios.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridViewUsuarios.Size = new System.Drawing.Size(357, 107);
             this.dataGridViewUsuarios.TabIndex = 5;
             // 
-            // toolStripButtonVolver
+            // button1
             // 
-            this.toolStripButtonVolver.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.toolStripButtonVolver.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonVolver.Image")));
-            this.toolStripButtonVolver.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButtonVolver.Name = "toolStripButtonVolver";
-            this.toolStripButtonVolver.Size = new System.Drawing.Size(23, 22);
-            this.toolStripButtonVolver.Text = "Volver";
-            this.toolStripButtonVolver.Click += new System.EventHandler(this.toolStripButtonVolver_Click);
+            this.button1.Location = new System.Drawing.Point(16, 127);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Añadir Usuario";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // FormUsuarios
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(222, 286);
+            this.ClientSize = new System.Drawing.Size(386, 286);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridViewUsuarios);
             this.Controls.Add(this.textBoxContraseña);
             this.Controls.Add(this.labelContraseña);
@@ -146,6 +138,7 @@
             this.Controls.Add(this.toolStrip1);
             this.Name = "FormUsuarios";
             this.Text = "FormUsuarios";
+            this.Load += new System.EventHandler(this.FormUsuarios_Load);
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewUsuarios)).EndInit();
@@ -157,8 +150,6 @@
         #endregion
 
         private System.Windows.Forms.ToolStrip toolStrip1;
-        private System.Windows.Forms.ToolStripButton toolStripButton1;
-        private System.Windows.Forms.ToolStripButton toolStripButton2;
         private System.Windows.Forms.ToolStripButton toolStripButton3;
         private System.Windows.Forms.Label labelNombre;
         private System.Windows.Forms.TextBox textBoxNombre;
@@ -166,5 +157,6 @@
         private System.Windows.Forms.TextBox textBoxContraseña;
         private System.Windows.Forms.DataGridView dataGridViewUsuarios;
         private System.Windows.Forms.ToolStripButton toolStripButtonVolver;
+        private System.Windows.Forms.Button button1;
     }
 }
--- movieQ(escritorio)/movieQ/movieQ/FormUsuarios.cs
+++ movieQ(escritorio)/movieQ/movieQ/FormUsuarios.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace movieQ
 {
@@ -21,7 +22,117 @@ namespace movieQ
         {
             FormOpciones fo = new FormOpciones();
             fo.Show();
-            this.Hide();
+            this.Hide();      
         }
+
+        public void cargarUsuarios()
+        {
+            string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=cineq;";
+            string query = "SELECT id, name, email FROM users";
+            MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+            commandDatabase.CommandTimeout = 60;
+            MySqlDataReader reader;
+            DataTable miTabla = new DataTable();
+
+            //SqlConnection conexion = new SqlConnection();
+            //SqlCommand sentencia = new SqlCommand();
+            //SqlDataReader datos;
+            
+
+            //conexion.ConnectionString = "Data Source=172.16.48.128\\sqlexpress;Initial Catalog=hoteles_basico;" +
+            //    "Persist Security Info=True;User ID=sa;Password=cep";
+
+            //sentencia.Connection = conexion;
+            //sentencia.CommandText =
+            //    "select * from hoteles where id_ciudad = @ciudad; ";
+            //sentencia.Parameters.Clear();
+            //sentencia.Parameters.AddWithValue("@ciudad", comboBoxCiutats.SelectedValue);
+
+            databaseConnection.Open();
+            miTabla = new DataTable();
+            reader = commandDatabase.ExecuteReader();//solo para selects
+            miTabla.Load(reader);
+            dataGridViewUsuarios.DataSource = miTabla;
+
+            databaseConnection.Close();
+        }
+
+        private void FormUsuarios_Load(object sender, EventArgs e)
+        {
+            cargarUsuarios();
+        }
+
+        private void borrarUsuari(int id_usuari)
+        {
+            string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=cineq;";
+            string query = "delete from users where id = @id_usuari";
+
+            MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+            commandDatabase.CommandTimeout = 60;
+            MySqlDataReader reader;
+
+            try
+            {
+                // Abrir la base de datos
+                databaseConnection.Open();
+
+                commandDatabase.Parameters.AddWithValue("@id_usuari", id_usuari);
+
+                commandDatabase.ExecuteNonQuery();
+
+                // Cerrar la conexion
+                databaseConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                // Mostrar mensajes de error
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        public void altaUsuari(string nom_usuari, string password)//falta encriptar password
+        {
+            string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=cineq;";
+            string query = "insert into users (name, password) values (@nom_usuari, @password)";
+
+            MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+            commandDatabase.CommandTimeout = 60;
+            MySqlDataReader reader;
+
+            try
+            {
+                // Abrir la base de datos
+                databaseConnection.Open();
+
+                commandDatabase.Parameters.AddWithValue("@nom_usuari", nom_usuari);
+                commandDatabase.Parameters.AddWithValue("@password", password);
+
+                commandDatabase.ExecuteNonQuery();
+
+                // Cerrar la conexion
+                databaseConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                // Mostrar mensajes de error
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            borrarUsuari((int)dataGridViewUsuarios.SelectedRows[0].Cells[0].Value);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            altaUsuari(textBoxNombre.Text, textBoxContraseña.Text);
+        }
+
+      
     }
 }
